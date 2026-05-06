d613186fcf6ea7dcd891948d23a0cc94c8de844c
joksadda15-ux <joksadda15@gmail.com>
2026-05-06 23:37:15 +0600

    Update firebase.js

--- api/utils/firebase.js
+++ api/utils/firebase.js
@@ -2,7 +2,6 @@ const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
     try {
-        // Vercel Environment Variables থেকে আপনার Firebase JSON ডাটা নিচ্ছে
         const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount)
